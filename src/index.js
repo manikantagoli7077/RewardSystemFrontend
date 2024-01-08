998cfbf26c6d3c470d7bef7ae6b41350d3e61542
manikantagoli7077 <manikantagoli7077@gmail.com>
2024-01-08 16:01:52 +0530

    to cpu

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    
-      <Routes>
+    <App/>
+      {/* <Routes>
         <Route path="/*" element={<App/>}/>
-      </Routes>
+      </Routes> */}
   
     </BrowserRouter>
   </React.StrictMode>
